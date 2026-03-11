c7999edbdb4435184891ed783deb626bfcfb5c1a
Semen <semen.luscheev@bk.ru>
2026-03-11 20:09:54 +0300

    FIX: displ !FIX LATER: added weights

---
+++ weights.py
@@ -0,0 +1,119 @@
+Mr={
+    "H": 1.008,
+    "He": 4.0026,
+    "Li": 6.94,
+    "Be": 9.0122,
+    "B": 10.81,
+    "C": 12.011,
+    "N": 14.007,
+    "O": 15.999,
+    "F": 18.998,
+    "Ne": 20.180,
+    "Na": 22.990,
+    "Mg": 24.305,
+    "Al": 26.982,
+    "Si": 28.085,
+    "P": 30.974,
+    "S": 32.06,
+    "Cl": 35.45,
+    "K": 39.098,
+    "Ar": 39.948,
+    "Ca": 40.078,
+    "Sc": 44.956,
+    "Ti": 47.867,
+    "V": 50.942,
+    "Cr": 51.996,
+    "Mn": 54.938,
+    "Fe": 55.845,
+    "Ni": 58.693,
+    "Cu": 63.546,
+    "Zn": 65.38,
+    "Ga": 69.723,
+    "Ge": 72.63,
+    "As": 74.922,
+    "Se": 78.971,
+    "Br": 79.904,
+    "Kr": 83.80,
+    "Rb": 85.468,
+    "Sr": 87.62,
+    "Y": 88.906,
+    "Zr": 91.224,
+    "Nb": 92.906,
+    "Mo": 95.95,
+    "Tc": 98.0, # Atomic mass of the most stable isotope
+    "Ru": 101.07,
+    "Rh": 102.91,
+    "Pd": 106.42,
+    "Ag": 107.87,
+    "Cd": 112.41,
+    "In": 114.82,
+    "Sn": 118.71,
+    "Sb": 121.76,
+    "I": 126.90,
+    "Te": 127.60,
+    "Xe": 131.29,
+    "Cs": 132.91,
+    "Ba": 137.33,
+    "La": 138.91,
+    "Ce": 140.12,
+    "Pr": 140.91,
+    "Nd": 144.24,
+    "Pm": 145.0, # Atomic mass of the most stable isotope
+    "Sm": 150.36,
+    "Eu": 151.96,
+    "Gd": 157.25,
+    "Tb": 158.93,
+    "Dy": 162.50,
+    "Ho": 164.93,
+    "Er": 167.26,
+    "Tm": 168.93,
+    "Yb": 173.04,
+    "Lu": 174.97,
+    "Hf": 178.49,
+    "Ta": 180.95,
+    "W": 183.84,
+    "Re": 186.21,
+    "Os": 190.23,
+    "Ir": 192.22,
+    "Pt": 195.08,
+    "Au": 196.97,
+    "Hg": 200.59,
+    "Tl": 204.38,
+    "Pb": 207.2,
+    "Bi": 208.98,
+    "Po": 209.0, # Atomic mass of the most stable isotope
+    "At": 210.0, # Atomic mass of the most stable isotope
+    "Rn": 222.0, # Atomic mass of the most stable isotope
+    "Fr": 223.0, # Atomic mass of the most stable isotope
+    "Ra": 226.0, # Atomic mass of the most stable isotope
+    "Ac": 227.0, # Atomic mass of the most stable isotope
+    "Pa": 231.04,
+    "Th": 232.04,
+    "Np": 237.0, # Atomic mass of the most stable isotope
+    "U": 238.03,
+    "Am": 243.0, # Atomic mass of the most stable isotope
+    "Pu": 244.0, # Atomic mass of the most stable isotope
+    "Cm": 247.0, # Atomic mass of the most stable isotope
+    "Bk": 247.0, # Atomic mass of the most stable isotope
+    "Cf": 251.0, # Atomic mass of the most stable isotope
+    "Es": 252.0, # Atomic mass of the most stable isotope
+    "Fm": 257.0, # Atomic mass of the most stable isotope
+    "Md": 258.0, # Atomic mass of the most stable isotope
+    "No": 259.0, # Atomic mass of the most stable isotope
+    "Lr": 262.0, # Atomic mass of the most stable isotope
+    "Rf": 261.0, # Atomic mass of the most stable isotope
+    "Db": 262.0, # Atomic mass of the most stable isotope
+    "Sg": 266.0, # Atomic mass of the most stable isotope
+    "Bh": 264.0, # Atomic mass of the most stable isotope
+    "Hs": 269.0, # Atomic mass of the most stable isotope
+    "Mt": 278.0, # Atomic mass of the most stable isotope
+    "Ds": 281.0, # Atomic mass of the most stable isotope
+    "Rg": 280.0, # Atomic mass of the most stable isotope
+    "Cn": 285.0, # Atomic mass of the most stable isotope
+    "Nh": 284.0, # Atomic mass of the most stable isotope
+    "Fl": 289.0, # Atomic mass of the most stable isotope
+    "Mc": 288.0, # Atomic mass of the most stable isotope
+    "Lv": 293.0, # Atomic mass of the most stable isotope
+    "Ts": 294.0, # Atomic mass of the most stable isotope
+    "Og": 294.0 # Atomic mass of the most stable isotope
+}
